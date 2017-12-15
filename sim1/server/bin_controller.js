9e0f303a10a6aed4bc9f94e4829c994c2f5528f8
Dante Dufala <thelonelycomet@gmail.com>
2017-12-15 12:32:11 -0700

    Final Commit

---
+++ sim1/server/bin_controller.js
@@ -0,0 +1,38 @@
+module.exports = {
+    getBins: (req, res) => {
+        let db = req.app.get('db');
+        db.getBins([req.params.shelf])
+        .then((bins) => {
+            res.status(200).send(bins)
+        })
+        .catch(() => res.status(500).send())
+    },
+    getBin: (req, res) => {
+        const db = req.app.get('db');
+        db.getBin([req.params.shelf, req.params.bin])
+        .then((bin) => {
+            res.status(200).send(bin)
+            .catch(() => res.status(500).send())
+        })
+    },
+    updateBin: (req, res) => {
+        const db = req.app.get('db');
+        var {Name, Price, ImageURL} = req.body;
+        db.updateBin([req.params.shelf, req.params.bin, Name, Price, ImageURL])
+        .then(() => res.status(200).send()) //74HIJ
+        .catch(() => res.status(500).send())
+    },
+    createBin: (req, res) => {
+        const db = req.app.get('db');
+        let {Name, Price, ImageURL} = req.body;
+        db.createBin([req.params.shelf, Name, Price, ImageURL])
+        .then(() => res.status(200).send())
+        .catch(() => res.status(500).send())
+    },
+    deleteBin: (req, res) => {
+        const db = req.app.get('db');
+        db.deleteBin([req.params.shelf, req.params.bin])
+        .then(() => res.status(200).send())
+        .catch(() => res.status(500).send())
+    }
+}
--- sim1/server/server.js
+++ sim1/server/server.js
@@ -2,18 +2,27 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
     massive = require('massive');
+    var binsCtrl = require('./bin_controller');
     require('dotenv').config()
 
 
     //TOP MIDDLEWARE
     const app = express(); //express invoked
-    app.use(bodyParser.json());
+    app.use(bodyParser.json());//bodyparser 76F
     app.use(cors());
-    massive( process.env.CONNECTION_STRING ).then( db => app.set('db', db) );
+    massive( process.env.CONNECTION_STRING ).then( db => { 
+        console.log('Connected bruh')
+        app.set('db', db) 
+    });
     
 
-    //MIDDLEWARE
-
+    //MIDDLEWARE //Restful apis POST GET PUT DELETE 76C for restful apis
+    app.get("/api/:shelf", binsCtrl.getBins) //74D-1
+    app.get("/api/:shelf/:bin", binsCtrl.getBin)
+    app.put("/api/:shelf/:bin", binsCtrl.updateBin) //Updates Bin with put 74D-2
+    app.put("/api/createbin/:shelf/:bin", binsCtrl.updateBin)//updates bin with put using same sql
+    app.delete('/api/:shelf/:bin', binsCtrl.deleteBin) //74D-4
+    app.post("/api/:shelf", binsCtrl.createBin); //Working post creates bin 74D-3
 
 
     const port = 3000;
